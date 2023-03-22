3e1e47bd36b16de42c11b315c90e6aaf88824a0d
zero <35671110+zeroclutch@users.noreply.github.com>
2023-03-22 13:33:09 -0700

    Speed up slow pivot

--- main/main.ino
+++ main/main.ino
@@ -8,8 +8,8 @@
 #include "module.hpp"
 #include "robot.hpp"
 
-// #define STARTING_STATE SeekLineState // actual
-#define STARTING_STATE SeekButtonState // testing only
+#define STARTING_STATE SeekLineState // actual
+// #define STARTING_STATE SeekButtonState // testing only
 
 // Modules
 #include "tcs.hpp"
@@ -78,7 +78,7 @@ const uint8_t LED_PINS[10]{
 const float FORWARD_SPEED = 0.48; // should be 0.45
 const float REVERSE_SPEED = -1; // should be -1
 const float PIVOT_SPEED = 0.45; // should be 0.3
-const float PIVOT_SPEED_SLOW = 0.15;
+const float PIVOT_SPEED_SLOW = 0.22;
 
 // Instantiate modules
 TCS tcsModule;
